eea8d94351469ab5cadc3ab0b4b77217093f566c
Jingwen <wjingwen96@gmail.com>
2022-12-03 22:03:22 +0800

    update

--- 4. sorting techniques/max_heap.py
+++ 4. sorting techniques/max_heap.py
@@ -1,3 +1,67 @@
+def heapify(arr, i):
+    largest = i
+    l = left(i)
+    r = right(i)
+    
+    if l < len(arr) and arr[l] > arr[i]:
+        largest = l
+        
+    else:
+        largest = i
+        
+    if r < len(arr) and arr[r] > arr[largest]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, largest)
+
+def left(i):
+    return 2 * i + 1
+
+def right(i):
+    return 2 * i + 2
+
+# Function to insert an element into the tree
+def insert(array, num):
+    size = len(array)
+    if size == 0:
+        array.append(num)
+    else:
+        array.append(num)
+        for i in range((size // 2) - 1, -1, -1):
+            heapify(array, i)
+
+arr = []
+
+insert(arr, 12)
+insert(arr, 34)
+insert(arr, 99)
+insert(arr, 22)
+insert(arr, 67)
+insert(arr, 82)
+
+print ("the max-heap array is: " + str(arr))
+
+def delete_root(arr):
+    global n
+    lastElement = arr[n - 1] # find last element
+    arr[0] = lastElement # replace root element
+    n = n - 1 # reduce size of heap by 1
+    heapify(arr, 0) # heapify root node
+    
+
+def printArray(arr, n):
+    for i in range(n):
+        print(arr[i],end=" ")
+    print()
+ 
+n = len(arr)
+delete_root(arr)
+printArray(arr, n)
+
+# %%
+# Alternate max-heap for array
 def max_heap(mylist,i):
     l = left(i)
     r = right(i)
